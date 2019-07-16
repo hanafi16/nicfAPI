4a0725d07001285a5b86b6abf4ea4b4b587733ab
Hanafi <hanafi.ms@gmail.com>
2019-07-16 17:02:12 +0800

    workshop02

--- workshop02/main.js
+++ workshop02/main.js
@@ -13,8 +13,8 @@ console.info(`Using ${keys.mongo}`);
 
 const db = CitiesDB({  
 	connectionUrl: keys.mongo, 
-	databaseName: 'zips', 
-	collectionName: 'city'
+	databaseName: 'cities', 
+	collectionName: 'cities'
 });
 
 const app = express();
@@ -22,27 +22,87 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.set('etag', false);
 // Start of workshop
 
 // Mandatory workshop
 // TODO GET /api/states
+app.get('/api/states', (req, resp) => {
 
+	resp.type('application/json');
 
-
+	db.findAllStates()
+	.then(result => {
+		resp.status(200);
+		resp.set('X-Date', (new Date()).toUTCString());
+		resp.json(result);
+	})
+	.catch(error => {
+		resp.status(400);
+		resp.json(error);
+	})
+	
+});
 
 // TODO GET /api/state/:state
+app.get('/api/state/:state', (req, resp) => {
+	const stateAbbrev = req.params.state;
 
+	resp.type('application/json');
+
+	db.findAllStates()
+	.then(result => {
+		if (result.indexOf(stateAbbrev.toUpperCase()) < 0)
+		{
+			resp.status(400);
+			resp.json({error: `Not a valid state: ${stateAbbrev}`})
+			return;
+		}
+		return (db.findCitiesByState(stateAbbrev))
+	})
+	.then(result => {
+		resp.status(200);
+		resp.set('X-Date', (new Date()).toUTCString());
+		resp.json(result.map(v => `/api/city/${v}`));
+	})
+	.catch(error => {
+		resp.status(400);
+		resp.json({error:error});
+	})
+
+});
 
 
 
 // TODO GET /api/city/:cityId
+app.get('/api/city/:cityId', (req, resp) => {
+	const cityId = req.params.cityId;
+	resp.type('application/json');
 
+	db.findCityById(cityId)
+	.then(result => {
+		if (result.length == 0) {
+			resp.status(404);
+			resp.json({error: `Not a valid City ID: ${cityId}`})
+			return;
+		}
+		resp.status(200);
+		resp.set('X-Date', (new Date()).toUTCString());
+		resp.json(result);
+	})
+	.catch(error => {
+		resp.status(400);
+		resp.json({error:error});
+	})
+
+});
 
 
 // TODO POST /api/city
+app.post('/api/city', (req, resp) => {
+	const newCity = req.body;
 
-
-
+})
 
 // Optional workshop
 // TODO HEAD /api/state/:state
